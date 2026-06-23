1da01d4ccb547e3bc92fbbb557cbba9f086e412f
hanzalashaikh588 <hanzalashaikh588@gmail.com>
2026-06-23 20:53:19 +0500

    Add files via upload

---
+++ python day 20.py
@@ -0,0 +1,17 @@
+def calculateHmean( a ,b ):
+    Hmean=(a*b)/(a+b)
+    print(Hmean)
+def isgreater( a, b):
+    pass
+    print("1st number is greater than 2nd number"  if a > b else " 2nd number is greater than 1st number")
+def islesser( a,b):
+    pass
+
+a=8
+b=6
+isgreater(a,b)
+calculateHmean(a , b)
+c= 16
+d=19
+isgreater(c,d)
+calculateHmean(c ,d)
