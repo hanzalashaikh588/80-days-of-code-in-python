a8a65e8b9794479f6c59bc281b257394aa76f500
hanzalashaikh588 <hanzalashaikh588@gmail.com>
2026-07-07 20:14:37 +0500

    Add files via upload

---
+++ python day 29.py
@@ -0,0 +1,9 @@
+#Docstrings in python are used right below the function defining its usage and functionality.doctrings must be right under the function or else none will print
+def square(n):
+    '''Takes in a number n, and retruns square of n.'''
+n =int(input("enter the value of square: "))
+print(n**2)
+print(square.__doc__)
+# ZEN of python
+import this
+
