4bb87e6b87c954c7955808e572a5d837474efa99
hanzalashaikh588 <hanzalashaikh588@gmail.com>
2026-07-23 23:10:24 +0500

    Refactor exception handling and age calculator

--- python day 36.py
+++ python day 36.py
@@ -1,20 +1,21 @@
 # exception handling
-#a = input(("Enter your number : "))
-# print(f"Multiplication table of {a} is :")
-# try:
-#     for i in range(1,11):
-#         print(f"{int(a)} X {(i)} = {int(a)*i}")
-# except:
-#     print("some error has occured")
+a = input(("Enter your number : "))
+print(f"Multiplication table of {a} is :")
+try:
+    for i in range(1,11):
+        print(f"{int(a)} X {(i)} = {int(a)*i}")
+except:
+    print("some error has occured")
 
-# print('some imp lines of code')
-# print('end of program')
+print('some imp lines of code')
+print('end of program')
 # handling specific errors
-# try:
-#     num = int(input("Enter an integer : "))
-#     print(f"{num} is an integer")
-# except ValueError:
-#     print("number entered is not an integer")
+try:
+    num = int(input("Enter an integer : "))
+    print(f"{num} is an integer")
+except ValueError:
+    print("number entered is not an integer")
+#example of another error.
 def function_age_calculator():
     name=input("enter your name : ")
     try:
