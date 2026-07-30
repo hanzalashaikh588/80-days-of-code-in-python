0055b944bc38dfee2780a056a48a4fa9898691c0
hanzalashaikh588 <hanzalashaikh588@gmail.com>
2026-07-30 18:53:36 +0500

    Add files via upload

---
+++ python day 39.py
@@ -0,0 +1,9 @@
+# secret language program
+#normal language
+original = ("qoiThekay POIbasemak is at x 107 y 45 z -345.I lokneedker you to kioraidmkj the lpkbaseasd and bcvdestroyhgf it klpcompletelypak mkkjwithoutlko kjhanyonegas knowinbzx")
+reversed_str = "".join(reversed(original))
+print(reversed_str) 
+#decoded
+print("now for the decoded version")
+original_str = "".join(reversed(reversed_str))
+print(original_str) 
