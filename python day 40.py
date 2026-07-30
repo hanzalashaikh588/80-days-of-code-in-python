ea80be0acb88debf83806a4ddce6e7075e0877e9
hanzalashaikh588 <hanzalashaikh588@gmail.com>
2026-07-30 18:50:59 +0500

    Add files via upload

---
+++ python day 40.py
@@ -0,0 +1,12 @@
+#shorthand if else stateme
+a=3000
+b=3500
+print("a") if a>b else print("a=b") if a==b else print('b')
+
+c = 9 if a<b else 0 
+print("c")
+
+question1= print("what is the capital of france?")
+answer1= input("enter your answer : ").capitalize()
+ans1= "Paris"
+print("correct") if(answer1==ans1)  else print("incorrect")
