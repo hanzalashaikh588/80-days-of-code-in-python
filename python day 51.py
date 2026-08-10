6af60ae38ac1b574e8f1aa742c11f1d5436eb173
hanzalashaikh588 <hanzalashaikh588@gmail.com>
2026-08-10 18:56:22 +0500

    Add files via upload

---
+++ python day 51.py
@@ -0,0 +1,10 @@
+#lambda fucntions
+double = lambda x: x*2
+x = (int(input("enter your number : ")))
+print(double(x))
+cube = lambda x : x*x*x
+avg = lambda x,y,z: (x+y+z)/3
+print(avg(2,4,12))
+print(cube(7))
+
+ 
